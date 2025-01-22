d59f2fce3034689a009978e39572f50536f222e7
Oumar Diarra <stianman35@gmail.com>
2025-01-22 15:46:14 +0100

    ajout des modifications

--- Calculator.py
+++ Calculator.py
@@ -2,3 +2,10 @@ def soustraction(a, b):
     return a - b
 
 print("Résultat de 8 - 2 :", soustraction(8, 2))
+
+# Calculatrice simple
+print("Bienvenue dans l'application de calculatrice améliorée !")
+def addition(a, b):
+    return a + b
+
+print("Résultat de 3 + 5 :", addition(3, 5))
