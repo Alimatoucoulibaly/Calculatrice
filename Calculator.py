8d8526c3e3fb5c2f0fd9f6ccddb3e71cb0740ba4
alimatou <coilibalyalimatou.com>
2025-01-22 16:03:49 +0100

    modification du fichier calculator.py

--- Calculator.py
+++ Calculator.py
@@ -3,4 +3,8 @@ print("Bienvenue dans la calculatrice collaborative !")
 def addition(a, b):
     return a + b
 
-print("Résultat de 3 + 5 :", addition(3, 5))
+print("Résultat de 3 * 5 :", multiplication(3, 5))
+def multiplicatin(a, b):
+    return a * b
+
+print("Résultat de 3 * 5 :", multiplicatin(3, 5))
